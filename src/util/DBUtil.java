eb684d962645b1afa4f8c6d3ae8204933c7af547
ZhihaoGu <gu_zhi_hao@163.com>
2018-12-18 14:28:26 +0800

    some Utils

---
+++ src/util/DBUtil.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.sql.*;
+
+public class DBUtil {
+
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/javaMySQL?useSSL=false&useUnicode=true&characterEncoding=UTF-8";
+    static final String USER = "root";
+    static final String PASS = "000000";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void closePreparedStatement(PreparedStatement pstmt) {
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+
+}
